f68417bd4db6715cb1ecabf616113e2cac99a6ba
Diaconu Daniel <businessanuntro@gmail.com>
2026-04-18 13:49:11 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return HTMLResponse("<h1>OK</h1>")
+templates = Jinja2Templates(directory="templates")
+
+# ---------------- HOME ----------------
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+
+# ---------------- UPLOAD (placeholder acum) ----------------
+@app.post("/upload/")
+async def upload(file: UploadFile):
+    content = await file.read()
+
+    return {
+        "status": "ok",
+        "filename": file.filename,
+        "size": len(content)
+    }
